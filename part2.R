a6dc90709f51a76adbd2449db2179d8a6a608d83
Francesco Ignazio Re <francescoignazio97@gmail.com>
2018-03-17 14:12:01 -0400

    committing

--- part2.R
+++ part2.R
@@ -4,7 +4,7 @@ library(stringr)
 library(ggplot2)
 library(munsell)
 
-
+devtools::install_github()
 veg.1 <- read_xlsx("veg1.xlsx")
 
 
@@ -95,7 +95,6 @@ ex9 <- ex8%>% filter(!Value %in% c("(D)", "(Z)", "(NA)", NA)) %>%
 
 View(ex9)
 
-unique(ex10[,"Label"])
 
 
 View(ex11)
@@ -113,12 +112,12 @@ View(unique(ex4[,"Data"]))
 
 
 
-ex11 <- ex10 %>% filter(Label == "RESTRICTED USE CHEMICAL")
-
+ex11 <- ex9 %>% filter(Label == "RESTRICTED USE CHEMICAL")
+View(ex11 %>% select(Name, ID)%>% unique())
 #Analysis on Restricted Chemicals on Broccoli on Average
 
-Br1 <- ex10 %>% filter(Commodity == "BROCCOLI", `Info on Measurement`==" MEASURED IN LB" , Label == "RESTRICTED USE CHEMICAL") 
-
+Br1 <- ex9 %>% filter(Commodity == "BROCCOLI", `Info on Measurement`==" MEASURED IN LB" , Label == "RESTRICTED USE CHEMICAL") 
+View(Br1)
 Br1$Value <- as.numeric(Br1$Value)
 Br1$Year <- as.numeric(Br1$Year)
 Br1$Year <- as.factor(Br1$Year)
@@ -142,8 +141,8 @@ ggplot(Br1, aes( x=Name, y= Value, fill=Year))+
   geom_bar( position = "dodge",stat="identity") + coord_flip() +  scale_fill_manual(values = c("yellow","orange", "red", "brown")) 
 
 
-#ggplot(Br1, aes( x=Name, y= Value, fill=Year))+
-#  geom_bar( position=position_dodge(preserve="single"),stat="identity")
+ggplot(Br1, aes( x=Name, y= Value, fill=Year))+
+  geom_bar( position=position_dodge(preserve="single"),stat="identity")
 
 #This above doesn't run, to ask professor.
 
@@ -234,6 +233,10 @@ ggplot(grapht, mapping=aes(x= Name, y=`mean(Value)`, fill = Commodity)) +
 # DISULFOTON
 # EMAMECTIN BENZOATE
 
+#Data collected from https://comptox.epa.gov/dashboard
+# BETA-CYFLUTHRIN and ZETA-CYPERMETHRIN  
+#did not have any data downloadble regarding toxicity information
+
 
 df2 = read.csv("2.csv",sep = ";")
 df3 = read.csv("3.csv",sep = ";")
@@ -264,41 +267,102 @@ as.tibble(df13)
 as.tibble(df14)
 as.tibble(df15)
 
+View(df5)
 
-df2 <- df2 %>% mutate( Name= "BIFENTHRIN") %>% filter(SPECIES == "rat")
-df3 <- df3 %>% mutate( Name= "ESFENVALERATE") %>% filter(SPECIES == "rat")
-df4 <- df4 %>% mutate( Name= "IMIDACLOPRID") %>% filter(SPECIES == "rat")
-df5 <- df5 %>% mutate( Name= "LAMBDA-CYHALOTHRIN") %>% filter(SPECIES == "rat")
-df6 <- df6 %>% mutate( Name= "METHOMYL") %>% filter(SPECIES == "rat")
-df7 <- df7 %>% mutate( Name= "NALED") %>% filter(SPECIES == "rat")
-df8 <- df8 %>% mutate( Name= "PERMETHRIN") %>% filter(SPECIES == "rat")
-df10 <- df10 %>% mutate( Name= "CHLORANTRANILIPROLE") %>% filter(SPECIES == "rat")
-df11 <- df11 %>% mutate( Name= "CHLORPYRIFOS") %>% filter(SPECIES == "rat")
-df12 <- df12%>% mutate( Name= "DIAZINON") %>% filter(SPECIES == "rat")
-df13 <- df13 %>% mutate( Name= "PRONAMIDE") %>% filter(SPECIES == "rat")
-df14 <- df14 %>% mutate( Name= "DISULFOTON") %>% filter(SPECIES == "rat")
-df15 <- df15 %>% mutate( Name= "EMAMECTIN BENZOATE") %>% filter(SPECIES == "rat")
 
+df2 <- df2 %>% mutate( Name= "BIFENTHRIN") %>% filter(SPECIES == "earthworm" | SPECIES == "rat" )
+df3 <- df3 %>% mutate( Name= "ESFENVALERATE") %>% filter(SPECIES == "earthworm" | SPECIES == "rat")
+df4 <- df4 %>% mutate( Name= "IMIDACLOPRID") %>% filter(SPECIES == "earthworm" | SPECIES == "rat")
+df5 <- df5 %>% mutate( Name= "LAMBDA-CYHALOTHRIN") %>% filter(SPECIES == "earthworm" | SPECIES == "rat")
+df6 <- df6 %>% mutate( Name= "METHOMYL") %>% filter(SPECIES == "earthworm" | SPECIES == "rat")
+df7 <- df7 %>% mutate( Name= "NALED") %>% filter(SPECIES == "earthworm" | SPECIES == "rat")
+df8 <- df8 %>% mutate( Name= "PERMETHRIN") %>% filter(SPECIES == "earthworm" | SPECIES == "rat")
+df10 <- df10 %>% mutate( Name= "CHLORANTRANILIPROLE") %>% filter(SPECIES == "earthworm" | SPECIES == "rat")
+df11 <- df11 %>% mutate( Name= "CHLORPYRIFOS") %>% filter(SPECIES == "earthworm" | SPECIES == "rat")
+df12 <- df12%>% mutate( Name= "DIAZINON") %>% filter(SPECIES == "earthworm" | SPECIES == "rat")
+df13 <- df13 %>% mutate( Name= "PRONAMIDE") %>% filter(SPECIES == "earthworm" | SPECIES == "rat")
+df14 <- df14 %>% mutate( Name= "DISULFOTON") %>% filter(SPECIES == "earthworm" | SPECIES == "rat")
+df15 <- df15 %>% mutate( Name= "EMAMECTIN BENZOATE") %>% filter(SPECIES == "earthworm" | SPECIES == "rat")
+
+
+
+View(tox1)
 
 tox <- rbind(df2,df3,df4,df5,df6,df7,df8,df10,df11,df12,df13,df14,df15)
 
+tox <- tox %>% filter(TYPE=="LC50" | TYPE == "LD50")
+
+
 tox1 <- tox %>% filter(UNITS  == "mg/kg") %>% 
-  group_by(Name,RISK_ASSESSMENT,UNITS) %>%
-  summarize(round(mean(PRIORITY)),mean(VALUES)) %>%
-  rename("Risk Assessment on rats" = RISK_ASSESSMENT,
-         "Values for LD50 on rats" = `mean(VALUES)`, 
-         "Priority" = `round(mean(PRIORITY))`, 
-         "Units" = UNITS)
+  group_by(Name,SPECIES,UNITS) %>%
+  summarize(round(mean(PRIORITY)),round(mean(VALUES),3)) %>%
+  rename( "Values" = `mean(VALUES)`, 
+          "Priority" = `round(mean(PRIORITY))`, 
+          "Units" = UNITS)
+
+tox1 <- ungroup(tox1)
+
+tox1$SPECIES <- as.character(tox1$SPECIES)
+tox1$Units <- as.character(tox1$Units)
+tox1$Values <- as.numeric(tox1$Values)
+
+
+
+tox1 <- spread(tox1, key= SPECIES, value = Values )
+
+
+tox1 <- tox1 %>% rename("Values for LD50 on rats" = rat, "Values for LC50 on earthworms"=earthworm)
+
+tox1 %>% filter(!is.na(`Values for LD50 on rats`)) %>% ggplot(aes(x=Name, y=`Values for LD50 on rats`)) + 
+  geom_bar(stat="identity",fill="steelblue") + coord_flip() +
+  labs(x="Chemical",title = "Aver. Quantity of chemicals (mg/kg) causing mortality in rats")
+
+tox1 %>% filter(!is.na(`Values for LC50 on earthworms`)) %>% ggplot(aes(x=Name, y=`Values for LC50 on earthworms`)) + 
+  geom_bar(stat="identity",fill="steelblue") + coord_flip() +
+  labs(x="Chemical",title = "Aver. Quantity of chemicals causing mortality in earthworms")
+
+
+restr <- ex9 %>% filter(Label == "RESTRICTED USE CHEMICAL") %>% group_by(Name)
+
+tox1 <- tox1 %>% group_by(Name)
+
+tox1$Name <- as.character(tox1$Name)
+restr <- as.character(tox1$Name)
+
+ID <- c(128825, 90100, 59101, 57801, 32501,122806, 109303, 129099, 128897, 90301,34401, 109701,101701,118831,129064)
+
+
+left1 <- c("BETA-CYFLUTHRIN", NA, NA,NA,NA)
+
+
+left2 <-c("ZETA-CYPERMETHRIN",NA,NA,NA,NA)
+
+tox1
+
+tox1[nrow(tox1) + 1,] <- left1
+tox1[nrow(tox1) + 1,] <- left2
+
+View(restr)
+restr
+tox1 <- add_column(tox1, ID)
+
+tox1$ID <- as.numeric(tox1$ID)
+restr$ID <- as.numeric(restr$ID)
 
 View(tox1)
 
 
-ggplot(tox1, aes(x=Name, y=`Values for LD50 on rats`)) + 
-  geom_bar(stat="identity",fill="steelblue") + coord_flip() +
-  labs(x="Chemical",title = "Aver. Quantity of chemicals (mg/kg) causing mortality in rats")
+tab1 <- left_join(tox1,restr, by= "ID")
 
-restr <- ex10 %>% filter(Label == "RESTRICTED USE CHEMICAL")
-#???
-View(full_join(restr, tox1,by="Name"))
+View(tab1)
+tab1 <- select(tab1, - Name.y , -ID)
 
-help("inner_join")
+tab1 <- tab1[,c(7:16, 1,6,17,2,3 : 5)]
+
+
+
+rbind(y,tox1)
+
+
+View(inner_join(tox1, restr, by="Name", copy = TRUE))
+
